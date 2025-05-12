e23fd9e486935f7103833407e7061fc79d18cad9
Ben Kilinski <ben.kilinski@gmail.com>
2025-05-12 18:12:06 +0300

    tailwind import

--- app-with-frontegg/src/App.tsx
+++ app-with-frontegg/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
-import { useEffect } from 'react'; // descomentado
-import { useAuth, useLoginWithRedirect, ContextHolder } from "@frontegg/react";
+import { useEffect } from 'react';
+import { useAuth, useLoginWithRedirect } from "@frontegg/react";
+import HomePage from '../src/Homepage';
 
 function App() {
-  const { user, isAuthenticated } = useAuth();
+  const { isAuthenticated } = useAuth();
   const loginWithRedirect = useLoginWithRedirect();
 
   useEffect(() => {
@@ -12,25 +13,9 @@ function App() {
     }
   }, [isAuthenticated, loginWithRedirect]);
 
-  const logout = () => {
-    const baseUrl = ContextHolder.getContext().baseUrl;
-    window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
-  };
-
   return (
     <div className="App">
-      { isAuthenticated ? (
-        <div>
-          <img src={user?.profilePictureUrl} alt={user?.name}/>
-          <p>Logged in as: {user?.name}</p>
-          <button onClick={() => alert(user.accessToken)}>What is my access token?</button>
-          <button onClick={() => logout()}>Click to logout</button>
-        </div>
-      ) : (
-        <div>
-          <button onClick={() => loginWithRedirect()}>Click me to login</button>
-        </div>
-      )}
+      {isAuthenticated ? <HomePage /> : <p>Redirecionando para login...</p>}
     </div>
   );
 }
