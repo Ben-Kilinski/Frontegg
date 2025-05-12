187d00fc6eee3c1b311fad7f9a65d478c2ef056c
Ben Kilinski <ben.kilinski@gmail.com>
2025-05-12 15:09:31 +0300

    vite config

--- app-with-frontegg/src/App.tsx
+++ app-with-frontegg/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useEffect } from 'react'; // descomentado
+import { useAuth, useLoginWithRedirect, ContextHolder } from "@frontegg/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { user, isAuthenticated } = useAuth();
+  const loginWithRedirect = useLoginWithRedirect();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      loginWithRedirect();
+    }
+  }, [isAuthenticated, loginWithRedirect]);
+
+  const logout = () => {
+    const baseUrl = ContextHolder.getContext().baseUrl;
+    window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      { isAuthenticated ? (
+        <div>
+          <img src={user?.profilePictureUrl} alt={user?.name}/>
+          <p>Logged in as: {user?.name}</p>
+          <button onClick={() => alert(user.accessToken)}>What is my access token?</button>
+          <button onClick={() => logout()}>Click to logout</button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={() => loginWithRedirect()}>Click me to login</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- app-with-frontegg/src/main.tsx
+++ app-with-frontegg/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { FronteggProvider } from '@frontegg/react';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const contextOptions = {
+  baseUrl: 'https://staging-benki.frontegg.com',
+  clientId: '5bbfc3df-0fa1-4cdd-876c-9fa93d032445', 
+  appId: '11a4449f-2dd9-45f1-86eb-99ab4c657bf9'
+};
+
+const authOptions = {
+  keepSessionAlive: true
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <FronteggProvider 
+    contextOptions={contextOptions} 
+    hostedLoginBox={true}
+    authOptions={authOptions}>
+      <App />
+  </FronteggProvider>
+);
